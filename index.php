7b11ed5ed329060941a74c787743804e16ebf47c
Jonas Kvinge <jonas@jkvinge.net>
2021-04-11 20:43:41 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,291 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Strawberry Music Player</title>
+  <link rel="stylesheet" type="text/css" href="strawberry.css" />
+  <link rel="stylesheet" type="text/css" href="lightbox.css" />
+  <base target="_blank" />
+</head>
+
+<body>
+
+<div id="container">
+
+<div id="banner">
+<div id="logo"><img src="pictures/strawberry128.png" alt="Strawberry Music Player" width="128" height="128" border="0" /></div>
+<div id="headline">Strawberry Music Player</div>
+<div id="donate">
+  <a href="https://paypal.me/jonaskvinge" width="1" height="1" />
+  <img src="https://www.paypalobjects.com/en_US/NO/i/btn/btn_donateCC_LG.gif" border="0" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+  </a>
+</div>
+</div>
+
+<div class="clr"></div>
+
+<div id="text">
+<p>
+Strawberry is a music player and music collection organizer. It is a fork of Clementine released in 2018 aimed at music collectors and audiophiles. It's written in C++ using the Qt toolkit.<br />
+</p>
+<div class="round-box">
+<ul>
+<li>Play and organize music</li>
+<li>Supports WAV, FLAC, WavPack, Ogg Vorbis, Speex, MPC, TrueAudio, AIFF, MP4, MP3, ASF and Monkey's Audio</li>
+<li>Audio CD playback</li>
+<li>Native desktop notifications</li>
+<li>Playlists in multiple formats</li>
+<li>Advanced audio output and device configuration for bit-perfect playback on Linux</li>
+<li>Edit tags on music files</li>
+<li>Fetch tags from MusicBrainz</li>
+<li>Album cover art from <a href="https://www.last.fm/">Last.fm</a>, <a href="https://musicbrainz.org/">Musicbrainz</a>, <a href="https://www.discogs.com/">Discogs</a>, <a href="https://www.musixmatch.com/">Musixmatch</a>, <a href="https://www.deezer.com/">Deezer</a>, <a href="https://www.tidal.com/">Tidal</a>, <a href="https://www.qobuz.com/">Qobuz</a> and <a href="https://www.spotify.com/">Spotify</a></li>
+<li>Song lyrics from <a href="https://audd.io/">AudD</a>, <a href="https://genius.com/">Genius</a>, <a href="https://www.musixmatch.com/">Musixmatch</a>, <a href="http://www.chartlyrics.com/">ChartLyrics</a>, <a href="https://lyrics.ovh/">lyrics.ovh</a> and <a href="https://www.lololyrics.com/">lololyrics.com</a></li>
+<li>Support for multiple backends</li>
+<li>Audio analyzer</li>
+<li>Audio equalizer</li>
+<li>Transfer music to iPod, MTP or mass-storage USB player</li>
+<li>Scrobbler with support for <a href="https://www.last.fm/">Last.fm</a>, <a href="https://libre.fm/">Libre.fm</a> and <a href="https://listenbrainz.org/">ListenBrainz</a></li>
+<li>Streaming support for Subsonic and Tidal</li>
+</ul>
+</div>
+
+<p>
+Strawberry Music Player uses lyrics from <a href="https://audd.io/"><img src="pictures/audd-14.png" /> AudD</a>
+</p>
+
+<p>If you like Strawberry Music Player, please consider donating using <a href="https://github.com/sponsors/jonaski">GitHub Sponsors</a><br />
+Funding developers through GitHub Sponsors is one more way to contribute to open source projects you appreciate. it helps developers get the resources they need, and recognize contributors working behind the scenes to make open source better for everyone.<br />
+You can also make a one-time donation through <a href="https://paypal.me/jonaskvinge">PayPal</a><br />
+</p>
+
+<h1>Download</h1>
+
+<p>Version 0.9.2</p>
+
+<table border="0">
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2.tar.xz"><img src="pictures/source-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2.tar.xz">Source</a></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/Strawberry-0.9.2-Qt5-x86_64.AppImage"><img src="pictures/appimage-32x30.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/Strawberry-0.9.2-Qt5-x86_64.AppImage">AppImage Qt 5</a></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/Strawberry-0.9.2-Qt6-x86_64.AppImage"><img src="pictures/appimage-32x30.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/Strawberry-0.9.2-Qt6-x86_64.AppImage">AppImage Qt 6</a></td>
+    <td></td>
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.lp152.x86_64.rpm"><img src="pictures/opensuse-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.lp152.x86_64.rpm">openSUSE 15.2</a></td> 
+  </tr>
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.lp153.x86_64.rpm"><img src="pictures/opensuse-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.lp153.x86_64.rpm">openSUSE 15.3</a></td> 
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.fc32.x86_64.rpm"><img src="pictures/fedora-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.fc32.x86_64.rpm">Fedora 32</a></td>
+  </tr>
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.fc33.x86_64.rpm"><img src="pictures/fedora-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.fc33.x86_64.rpm">Fedora 33</a></td>
+  </tr>
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.fc34.x86_64.rpm"><img src="pictures/fedora-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.fc34.x86_64.rpm">Fedora 34</a></td>
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.mga7.x86_64.rpm"><img src="pictures/mageia-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.mga7.x86_64.rpm">Mageia 7</a></td>
+  </tr>
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.mga8.x86_64.rpm"><img src="pictures/mageia-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-1.mga8.x86_64.rpm">Mageia 8</a></td>
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry_0.9.2-buster_amd64.deb"><img src="pictures/debian-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry_0.9.2-buster_amd64.deb">Debian Buster</a></td> 
+  </tr>
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry_0.9.2-bullseye_amd64.deb"><img src="pictures/debian-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry_0.9.2-bullseye_amd64.deb">Debian Bullseye</a></td> 
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry_0.9.2-bionic_amd64.deb"><img src="pictures/ubuntu-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry_0.9.2-bionic_amd64.deb">Ubuntu Bionic</a></td>
+  </tr>
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry_0.9.2-focal_amd64.deb"><img src="pictures/ubuntu-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry_0.9.2-focal_amd64.deb">Ubuntu Focal</a></td>
+  </tr>
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry_0.9.2-groovy_amd64.deb"><img src="pictures/ubuntu-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry_0.9.2-groovy_amd64.deb">Ubuntu Groovy</a></td>
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-mojave-x86_64.dmg"><img src="pictures/macos-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-mojave-x86_64.dmg">macOS Mojave</a></td>
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-catalina-x86_64.dmg"><img src="pictures/macos-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-catalina-x86_64.dmg">macOS Catalina</a></td>
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-bigsur-x86_64.dmg"><img src="pictures/macos-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/strawberry-0.9.2-bigsur-x86_64.dmg">macOS Big Sur</a></td>
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/StrawberrySetup-0.9.2-Qt5-x64.exe"><img src="pictures/windows-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/StrawberrySetup-0.9.2-Qt5-x64.exe">Windows (7/8.1/10) 64 bit (Qt 5)</a></td>
+  </tr>
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/StrawberrySetup-0.9.2-Qt6-x64.exe"><img src="pictures/windows-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/StrawberrySetup-0.9.2-Qt6-x64.exe">Windows (7/8.1/10) 64 bit (Qt 6)</a></td>
+  </tr>
+
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/StrawberrySetup-0.9.2-Qt5-x86.exe"><img src="pictures/windows-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/StrawberrySetup-0.9.2-Qt5-x86.exe">Windows (7/8.1/10) 32 bit (Qt 5)</a></td>
+  </tr>
+  <tr>
+    <td><a href="https://files.strawberrymusicplayer.org/StrawberrySetup-0.9.2-Qt6-x86.exe"><img src="pictures/windows-32x32.png" /></a></td>
+    <td><a href="https://files.strawberrymusicplayer.org/StrawberrySetup-0.9.2-Qt6-x86.exe">Windows (7/8.1/10) 32 bit (Qt 6)</a></td>
+  </tr>
+
+</table>
+
+<p>
+For technical help, questions and discussion join our forum on
+<a href="https://forum.strawberrymusicplayer.org/">https://forum.strawberrymusicplayer.org/</a><br />
+</p>
+
+<p>
+You can obtain and view the sourcecode, open issues and join the development on github at:
+<a href="https://github.com/strawberrymusicplayer/strawberry">https://github.com/strawberrymusicplayer/strawberry</a><br />
+</p>
+
+<p>Newer exprimental development builds can be found for openSUSE, Fedora, Mageia, Ubuntu, Debian, macOS and Windows on <a href="http://builds.strawberrymusicplayer.org/">http://builds.strawberrymusicplayer.org/</a></p>
+
+<p>There currently isn't any macOS developers actively working on this project. If you want to join the development and contribute, join us on <a href="https://github.com/strawberrymusicplayer/strawberry">GitHub</a></p>
+
+<h1>Repositories</h1>
+
+<p>Repositories containing the latest development releases are available for openSUSE, Fedora and Mageia through <a href="https://download.opensuse.org/repositories/home:/jonaski:/audio/">openSUSE buildservice</a>
+</p>
+
+<p>Ubuntu and possible Ubuntu clones can use the PPA respository:<br />
+
+sudo add-apt-repository ppa:jonaski/strawberry<br />
+sudo apt-get update<br />
+sudo apt-get install strawberry<br />
+
+</p>
+
+<h1>Screenshots</h1>
+
+<div class="screenshots">
+
+<table>
+<tr class="screenshots-picture">
+<td class="screenshots-picture">
+<a href="pictures/screenshot-001-large.png" data-lightbox="screenshot"><img src="pictures/screenshot-001-thumb.png" alt="Screenshot #1" /></a>
+</td>
+<td class="screenshots-picture">
+<a href="pictures/screenshot-002-large.png" data-lightbox="screenshot"><img src="pictures/screenshot-002-thumb.png" alt="Screenshot #2" /></a>
+</td>
+<td class="screenshots-picture">
+<a href="pictures/screenshot-003-large.png" data-lightbox="screenshot"><img src="pictures/screenshot-003-thumb.png" alt="Screenshot #3" /></a>
+</td>
+</tr>
+<tr class="screenshots-text">
+<td class="screenshots-text">Main window with collection information. The GUI is clean and simple.</td>
+<td class="screenshots-text">Context with song information and lyrics.</td>
+<td class="screenshots-text">Collection with song playing.</td>
+</tr>
+</table>
+
+<table>
+<tr class="screenshots-picture">
+<td class="screenshots-picture">
+<a href="pictures/screenshot-004-large.png" data-lightbox="screenshot"><img src="pictures/screenshot-004-thumb.png" alt="Screenshot #4" /></a>
+</td>
+<td class="screenshots-picture">
+<a href="pictures/screenshot-005-large.png" data-lightbox="screenshot"><img src="pictures/screenshot-005-thumb.png" alt="Screenshot #5" /></a>
+</td>
+<td class="screenshots-picture">
+<a href="pictures/screenshot-006-large.png" data-lightbox="screenshot"><img src="pictures/screenshot-006-thumb.png" alt="Screenshot #6" /></a>
+</td>
+</tr>
+<tr class="screenshots-text">
+<td class="screenshots-text">Tidal search.</td>
+<td class="screenshots-text">Playing high resolution audio in 24-bit 96kHz sample rate without resampling.</td>
+<td class="screenshots-text">Album Cover Manager for managing album covers for your albums.</td>
+</tr>
+</table>
+
+<table>
+<tr class="screenshots-picture">
+<td class="screenshots-picture">
+<a href="pictures/screenshot-007-large.png" data-lightbox="screenshot"><img src="pictures/screenshot-007-thumb.png" alt="Screenshot #7" /></a>
+</td>
+<td class="screenshots-picture">
+<a href="pictures/screenshot-008-large.png" data-lightbox="screenshot"><img src="pictures/screenshot-008-thumb.png" alt="Screenshot #8" /></a>
+</td>
+<td class="screenshots-picture">
+<a href="pictures/screenshot-009-large.png" data-lightbox="screenshot"><img src="pictures/screenshot-009-thumb.png" alt="Screenshot #9" /></a>
+</td>
+</tr>
+<tr class="screenshots-text">
+<td class="screenshots-text">Collection settings.</td>
+<td class="screenshots-text">Advanced backend settings on Linux. Selecting output device will play without resampling.</td>
+<td class="screenshots-text">Tidal settings.</td>
+</tr>
+</table>
+
+</div>
+
+<br />
+
+<h1>How to help spread the word</h1>
+
+<p>
+<ul>
+<li>Star the project on <a href="https://github.com/strawberrymusicplayer/strawberry">GitHub</a></li>
+<li>Ask your distro to package it</li>
+<li>Vote for it on <a href="https://alternativeto.net/software/strawberry/">AlternativeTo</a></li>
+<li>Like and share it on <a href="https://www.facebook.com/strawberrymusicplayer/">Facebook</a></li>
+<li>Vote for it on <a href="https://www.slant.co">Slant</a></li>
+</ul>
+</p>
+
+<p>
+Copyright &copy; 2013-2020 Jonas Kvinge<br />
+Verbatim copying and redistribution of this entire page are permitted provided this notice is preserved.
+</p>
+
+</div>
+</div>
+
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="lightbox.js"></script>
+
+</body>
+</html>
